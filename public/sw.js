bad53abdeb043cb18fb6b6d4c2861884a87175c0
Alexis Rodríguez <alexis.rcas@gmail.com>
2025-10-17 23:08:00 +0100

    Mover serviceworker

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,18 @@
 const CACHE_NAME = 'mi-cache-v1';
+
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
   '/assets/js/main.js',
-  '/assets/css/style.css',
+  '/assets/css/main.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js',
   'https://unpkg.com/lit@3.2.0/index.js?module'
 ];
+
+
+
 
 self.addEventListener('install', event => {
   event.waitUntil(
